5a6c68ab2562e996f2c9bdc0b003e7d6675d7c3f
Daniel Koman <dankoman30@gmail.com>
2015-09-02 15:11:46 +0200

    PrebuiltGmsCore variant naming scheme changes for lollipop
    4** becomes 2** for lollipop (4** now reserved for marshmallow)

--- structure/dynamic.sh
+++ structure/dynamic.sh
@@ -4,11 +4,11 @@
 ARCH=$(grep ro.product.cpu.abi= /system/build.prop | cut -d "=" -f 2);
 
 # PrebuiltGmsCore
-# COPY 430 (ARM VERSION) TO SYSTEM
-cp -af /tmp/PrebuiltGmsCore/430/* /system
+# COPY 230 (ARM VERSION) TO SYSTEM
+cp -af /tmp/PrebuiltGmsCore/230/* /system
 
-# IF 64-BIT ARCHITECTURE, REMOVE 430 AND INSTALL 440
+# IF 64-BIT ARCHITECTURE, REMOVE 230 AND INSTALL 240
 if [ $ARCH == arm64-v8a ]; then
  rm -rf /system/priv-app/PrebuiltGmsCore
- cp -af /tmp/PrebuiltGmsCore/440/* /system
+ cp -af /tmp/PrebuiltGmsCore/240/* /system
 fi
